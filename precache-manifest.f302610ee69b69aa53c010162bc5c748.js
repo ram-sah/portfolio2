d6f980399c7226d5d700a04fc4988f12638faa98
Ram Sah <mohan2036@gmail.com>
2020-12-06 18:29:38 -0500

    initial push

---
+++ precache-manifest.f302610ee69b69aa53c010162bc5c748.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6297e07f07dfec54e0c4dc6a185bf3d3",
+    "url": "/React-Portfolio/index.html"
+  },
+  {
+    "revision": "ef747200020326785b79",
+    "url": "/React-Portfolio/static/css/2.59bc9275.chunk.css"
+  },
+  {
+    "revision": "a9f9ab919b579599b0c2",
+    "url": "/React-Portfolio/static/css/main.14629533.chunk.css"
+  },
+  {
+    "revision": "ef747200020326785b79",
+    "url": "/React-Portfolio/static/js/2.e098f58f.chunk.js"
+  },
+  {
+    "revision": "c67c13e3d7b57854efcfe3f4623b2160",
+    "url": "/React-Portfolio/static/js/2.e098f58f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9f9ab919b579599b0c2",
+    "url": "/React-Portfolio/static/js/main.8785cb75.chunk.js"
+  },
+  {
+    "revision": "0274f599536680666069",
+    "url": "/React-Portfolio/static/js/runtime-main.27e0f504.js"
+  },
+  {
+    "revision": "9e0eb877897549a25caf00257abae4df",
+    "url": "/React-Portfolio/static/media/RAM-resume.9e0eb877.pdf"
+  },
+  {
+    "revision": "3d0e31dda299a46ffc6b4827eb383bd7",
+    "url": "/React-Portfolio/static/media/burger.3d0e31dd.gif"
+  },
+  {
+    "revision": "61853f6a46824403928bb6a77d8579dc",
+    "url": "/React-Portfolio/static/media/employee-directory.61853f6a.png"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/React-Portfolio/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/React-Portfolio/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/React-Portfolio/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/React-Portfolio/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/React-Portfolio/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "0d28c6b2a2eb3d81bd5d99b3f9a9c484",
+    "url": "/React-Portfolio/static/media/google-Book.0d28c6b2.png"
+  },
+  {
+    "revision": "73766e66a8cffe1f6912b1a0804a96b4",
+    "url": "/React-Portfolio/static/media/project-2.73766e66.png"
+  },
+  {
+    "revision": "5c7a6bbe3e966a87d9b39e8b3a01ba8d",
+    "url": "/React-Portfolio/static/media/project-3.5c7a6bbe.png"
+  },
+  {
+    "revision": "f60ab2092edfa88bae9a19626b5f2098",
+    "url": "/React-Portfolio/static/media/ram.f60ab209.jpg"
+  },
+  {
+    "revision": "833821bc475e2add1a7aabe593648a7c",
+    "url": "/React-Portfolio/static/media/weather.833821bc.jpg"
+  }
+]);
